64f307e0db9cf106ac4c32a8a23ef4b4e525703a
comavp <comavp@yandex.ru>
2022-01-24 12:49:47 +0400

    Add page for designing tacos

--- taco-cloud/src/main/java/com/example/tacocloud/controller/DesignTacoController.java
+++ taco-cloud/src/main/java/com/example/tacocloud/controller/DesignTacoController.java
@@ -2,6 +2,7 @@ package com.example.tacocloud.controller;
 
 import com.example.tacocloud.Ingredient;
 import com.example.tacocloud.Ingredient.Type;
+import com.example.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,15 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    /*@GetMapping
-    public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
+    @GetMapping
+    public String showDesignForm(final Model model) {
+        final List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
                 new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
@@ -31,7 +33,10 @@ public class DesignTacoController {
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
         Arrays.stream(Type.values()).forEach(type -> model.addAttribute(type.toString().toLowerCase(),
-                filterByType(ingredients, type)));
+                ingredients.stream().filter(ingredient -> ingredient.getType().equals(type)).collect(Collectors.toList())));
+
         model.addAttribute("design", new Taco());
-    }*/
+
+        return "design";
+    }
 }
