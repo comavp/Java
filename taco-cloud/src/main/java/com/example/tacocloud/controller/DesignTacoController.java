a7fb890c501857cb9b38e6b9f5cd95db04fb11e7
comavp <comavp@yandex.ru>
2022-01-31 21:59:16 +0400

    Add validation errors

--- taco-cloud/src/main/java/com/example/tacocloud/controller/DesignTacoController.java
+++ taco-cloud/src/main/java/com/example/tacocloud/controller/DesignTacoController.java
@@ -6,10 +6,12 @@ import com.example.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -43,7 +45,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(final Taco taco) {
+    public String processDesign(@Valid final Taco taco, final Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         // todo save the taco design
         log.info("Processing design: " + taco);
 
