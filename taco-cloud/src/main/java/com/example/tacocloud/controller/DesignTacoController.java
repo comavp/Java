21ca602548ad533db6a4259aff17b0d198a175bd
comavp <comavp@yandex.ru>
2022-02-05 00:33:23 +0400

    Add layer of working with data using 'JdbcTemplate'

--- taco-cloud/src/main/java/com/example/tacocloud/controller/DesignTacoController.java
+++ taco-cloud/src/main/java/com/example/tacocloud/controller/DesignTacoController.java
@@ -3,7 +3,9 @@ package com.example.tacocloud.controller;
 import com.example.tacocloud.model.Ingredient;
 import com.example.tacocloud.model.Ingredient.Type;
 import com.example.tacocloud.model.Taco;
+import com.example.tacocloud.repository.IngredientRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,18 +24,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    public final static List<Ingredient> TEST_INGREDIENTS = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-    );
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(final IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
 
     @GetMapping
     public String showDesignForm(final Model model) {
@@ -57,7 +54,10 @@ public class DesignTacoController {
     }
 
     private void addIngredientsIntoDesignView(final Model model) {
+        final List<Ingredient> allIngredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(allIngredients::add);
+
         Arrays.stream(Type.values()).forEach(type -> model.addAttribute(type.toString().toLowerCase(),
-                TEST_INGREDIENTS.stream().filter(ingredient -> ingredient.getType().equals(type)).collect(Collectors.toList())));
+                allIngredients.stream().filter(ingredient -> ingredient.getType().equals(type)).collect(Collectors.toList())));
     }
 }
--- taco-cloud/src/main/java/com/example/tacocloud/model/IngredientByIdConverter.java
+++ taco-cloud/src/main/java/com/example/tacocloud/model/IngredientByIdConverter.java
@@ -1,16 +1,25 @@
 package com.example.tacocloud.model;
 
-import com.example.tacocloud.model.Ingredient;
+import com.example.tacocloud.repository.IngredientRepository;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import static com.example.tacocloud.controller.DesignTacoController.TEST_INGREDIENTS;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientByIdConverter(final IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
     @Override
     public Ingredient convert(final String typeId) {
-        return TEST_INGREDIENTS.stream().filter(ingredient -> typeId.equals(ingredient.getId())).findFirst().get();
+        final List<Ingredient> allIngredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(allIngredients::add);
+        return allIngredients.stream().filter(ingredient -> typeId.equals(ingredient.getId())).findFirst().get();
     }
 }
