f9a5445b52a051c37feae6eb53583a1c2a70192e
comavp <comavp@yandex.ru>
2022-02-15 21:11:09 +0400

    Add Spring Data Jpa

--- taco-cloud/src/main/java/com/example/tacocloud/model/Taco.java
+++ taco-cloud/src/main/java/com/example/tacocloud/model/Taco.java
@@ -2,23 +2,35 @@ package com.example.tacocloud.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
     // todo не отображается валидационные сообщения
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private Date createdAt;
 
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+
+    @ManyToMany(targetEntity = Ingredient.class)
     @NotNull(message="You must choose at least 1 ingredient")
     @Size(min=1)
     private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }
