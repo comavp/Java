c40a614009ae222a76e696fbf46a72526871b688
comavp <comavp@yandex.ru>
2022-07-05 20:20:39 +0400

    Add api-gateway-service

---
+++ FirstMicroservicesProject/api-gateway-service/src/main/java/ru/comavp/apigateway/SpringDemoApigatewayApplication.java
@@ -0,0 +1,12 @@
+package ru.comavp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApigatewayApplication.class, args);
+    }
+}
