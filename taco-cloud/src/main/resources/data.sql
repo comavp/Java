29847db02b885647170ddcc865ac3d34ba16747f
comavp <comavp@yandex.ru>
2022-07-05 20:55:13 +0400

    Add example of JDBC-based user store Spring Security configuration

--- taco-cloud/src/main/resources/data.sql
+++ taco-cloud/src/main/resources/data.sql
@@ -14,3 +14,22 @@ insert into Ingredient (id, name, type) values ('CHED', 'Cheddar', 'CHEESE');
 insert into Ingredient (id, name, type) values ('JACK', 'Monterrey Jack', 'CHEESE');
 insert into Ingredient (id, name, type) values ('SLSA', 'Salsa', 'SAUCE');
 insert into Ingredient (id, name, type) values ('SRCR', 'Sour Cream', 'SAUCE');
+
+CREATE TABLE users (
+  id VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  enabled TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (id)
+);
+
+insert into Users (id, username, password, enabled) values ('1', 'Anton', '1234', '1');
+
+CREATE TABLE UserAuthorities (
+  id VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
+  FOREIGN KEY (username) REFERENCES users(username)
+);
+
+insert into UserAuthorities (id, username, authority) values ('1', 'Anton', 'admin');
