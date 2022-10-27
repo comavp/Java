33a4a4dbeee4126cbb1febeb87933c3fc7893a9d
comavp <comavp@yandex.ru>
2022-10-27 20:02:25 +0400

    Show example of using default schema for jdbc-based Spring Security

--- taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.tacocloud.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import javax.sql.DataSource;
@@ -20,12 +24,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .usersByUsernameQuery(
-                        "select username, password, enabled from Users where username=?"
+                .withDefaultSchema()
+                .withUser(User.withUsername("Jack")
+                        .password(passwordEncoder().encode("1234"))
+                        .roles("USER")
                 )
-                .authoritiesByUsernameQuery(
-                        "select username, authority from UserAuthorities where username=?"
-                )
-                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
