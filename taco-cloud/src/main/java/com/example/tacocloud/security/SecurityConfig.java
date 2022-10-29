2aa83c7c86a99da1aca00986b1d7e593b5365b40
comavp <comavp@yandex.ru>
2022-10-29 21:53:49 +0400

    Add example of LDAP-based user store Spring Security configuration

--- taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -1,39 +1,37 @@
 package com.example.tacocloud.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    DataSource dataSource;
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser(User.withUsername("Jack")
-                        .password(passwordEncoder().encode("1234"))
-                        .roles("USER")
-                )
-                .passwordEncoder(passwordEncoder());
+        auth.ldapAuthentication()
+                .contextSource()
+                    .root("dc=tacocloud,dc=com")
+                    .ldif("classpath:users.ldif")
+                .and()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                .passwordCompare()
+                    .passwordEncoder(passwordEncoder())
+                    .passwordAttribute("userPassword");
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        //return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
