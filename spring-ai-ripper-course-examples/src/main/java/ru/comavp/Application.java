9092680b20bcb570044b2e4ee8c5a9fe5d44ef39
comavp <comavp@yandex.ru>
2025-11-02 23:58:03 +0400

    Add initial version of spring-ai-ripper project

---
+++ spring-ai-ripper-course-examples/src/main/java/ru/comavp/Application.java
@@ -0,0 +1,20 @@
+package ru.comavp;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+        ChatClient chatClient = SpringApplication.run(Application.class, args).getBean(ChatClient.class);
+        System.out.println(chatClient.prompt().user("Дай первую строку богемской рапсодии").call().content());
+    }
+}
