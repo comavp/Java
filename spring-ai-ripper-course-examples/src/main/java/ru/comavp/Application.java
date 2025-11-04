6d6d3487ba5352fcadea7456d0bc7a5e0a2d09a0
comavp <comavp@yandex.ru>
2025-11-04 21:52:51 +0400

    [WIP] Add integration with LMStudio

--- spring-ai-ripper-course-examples/src/main/java/ru/comavp/Application.java
+++ spring-ai-ripper-course-examples/src/main/java/ru/comavp/Application.java
@@ -1,20 +1,46 @@
 package ru.comavp;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @SpringBootApplication
 public class Application {
 
+//    @Bean
+//    public HttpClient httpClient() {
+//        return HttpClient.newBuilder()
+//                .version(HttpClient.Version.HTTP_1_1)
+//                .connectTimeout(Duration.ofSeconds(5))
+//                .build();
+//    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
+        return builder
+                .defaultOptions(ChatOptions.builder()
+                        .model("qwen3-30b-a3b")
+                        .build())
+                .build();
     }
 
     public static void main(String[] args) {
-        ChatClient chatClient = SpringApplication.run(Application.class, args).getBean(ChatClient.class);
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        ChatClient chatClient = context.getBean(ChatClient.class);
+        System.out.println("Waiting for LLM response ...");
         System.out.println(chatClient.prompt().user("Дай первую строку богемской рапсодии").call().content());
     }
 }
